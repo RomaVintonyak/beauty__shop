9e4b17ee56f3e9aeec247b9ba6fde4479560480f
Roma Vintonyak <19roman23@gmail.com>
2024-03-05 18:02:30 +0200

    create header section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,9 @@
 jQuery(document).ready(function () {
   "use script";
-
+  /*burger button*/
+  var burger = $(".burger__btn");
+  burger.on("click", function(event){
+    event.preventDefault();
+    $(this).toggleClass("burger__btn--active");
+  })
 });
