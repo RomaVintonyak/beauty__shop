52e5de6aa6a61877c5d4ccb021cf6072e4bcf232
Roma Vintonyak <19roman23@gmail.com>
2024-03-07 19:42:17 +0200

    create footer section

--- app/js/main.js
+++ app/js/main.js
@@ -14,8 +14,28 @@ jQuery(document).ready(function () {
     autoplay: false,
     dotsClass: "intro__slider--dots",
     draggable: false,
+    touchMove: false,
+    focusOnSelect: false,
+    swipe: false,
     arrows: true,
     prevArrow: '<button type="button" class="intro__slider--prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow: '<button type="button" class="intro__slider--next"><i class="fas fa-chevron-right"></i></button>',
+  });
+  /*main product slider*/
+  var productSlider = $("#productSlider");
+  productSlider.slick({
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    draggable: false,
+    touchMove: false,
+    focusOnSelect: false,
+    swipe: false,
+    arrows: true,
+    prevArrow: '<button type="button" class="product__slider--prev"><span></span></button>',
+    nextArrow: '<button type="button" class="product__slider--next"><span></span></button>',
   });
 });
