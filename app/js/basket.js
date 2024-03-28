c1539c1a73661dd8b3a2fe85e1951e0e9b662c6c
Roma Vintonyak <19roman23@gmail.com>
2024-03-28 19:40:13 +0200

    create basket page

--- app/js/basket.js
+++ app/js/basket.js
@@ -1,25 +1,82 @@
 jQuery(document).ready(function () {
    "use script";
    /*tovar count item*/
-   var tovarPrice = $("._tovarPrice").text();
-   var tovarCount = $("._tovarCount").val().trim();
-   var tovarSumm = tovarPrice * tovarCount;
-   $("._tovarSumm").html(tovarSumm);
+   $("._tovarPrice").each(function () {
+      var tovarPrice = $(this).text();
+      var tovarCount = $(this).closest(".tovar__opys")
+         .closest(".tovar__block--content").find("._tovarCount").val();
+      var tovarRezult = tovarPrice * tovarCount;
+      var tovarSumm = $(this).closest(".tovar__opys")
+         .closest(".tovar__block--content").find("._tovarSumm");
+      tovarSumm.html(tovarRezult);
+   });
+   /*all basket summ*/
+   function allSumm() {
+      var arrList = $('._tovarSumm').map(function () {
+         var arrTovarSumm = $(this).text();
+         var numberTovarSumm = Number(arrTovarSumm);
+         return numberTovarSumm;
+      }).get();
+      function arraySum(array) {
+         var summaTovariv = 0;
+         for (var i = 0; i < array.length; i++) {
+            summaTovariv += array[i];
+         }
+         $("._basketSumm").html(summaTovariv);
+      }
+      arraySum(arrList);
+   }
+   allSumm();
+   /*remove product position*/
+   $("._removeBlock").on("click", function () {
+      if (confirm("Бажаєте видалити дану позицію?")) {
+         $(this).closest(".tovar__block--delete").closest(".tovar__block").remove();
+         allSumm();
+      }
+      else {
+         return false;
+      }
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
    /*count minus*/
-   $(".count__minus").on("click", function (event) {
+   /*$(".count__minus").on("click", function (event) {
       event.preventDefault();
       var counterMinus = $(this).closest(".tovar__count").find("._tovarCount");
+
+
       counterMinus.val(+counterMinus.val() - 1);
+
+
       var tovarPriceM = $(this).closest(".tovar__count")
          .closest(".tovar__block--content").find("._tovarPrice").text();
       var tovarCountM = $(this).closest(".tovar__count").find("._tovarCount").val().trim();
       var tovarSumm = tovarPriceM * tovarCountM;
       var tovarSummM = $(this).closest(".tovar__count")
          .closest(".tovar__block--content").find("._tovarSumm");
-         tovarSummM.html(tovarSumm);
+      tovarSummM.html(tovarSumm);
    });
    /*count plus*/
-   $(".count__plus").on("click", function (event) {
+   /*$(".count__plus").on("click", function (event) {
       event.preventDefault();
       var counertPlus = $(this).closest(".tovar__count").find("._tovarCount");
       counertPlus.val(+counertPlus.val() + 1);
@@ -28,7 +85,7 @@ jQuery(document).ready(function () {
       var tovarCountM = $(this).closest(".tovar__count").find("._tovarCount").val().trim();
       var tovarSumm = tovarPriceM * tovarCountM;
       var tovarSummM = $(this).closest(".tovar__count")
-      .closest(".tovar__block--content").find("._tovarSumm");
+         .closest(".tovar__block--content").find("._tovarSumm");
       tovarSummM.html(tovarSumm);
-   });
+   });*/
 });
