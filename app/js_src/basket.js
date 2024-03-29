381d7921303784d11e90b5104e02990f3260c9b6
Roma Vintonyak <19roman23@gmail.com>
2024-03-29 18:39:24 +0200

    finished basket page

--- app/js_src/basket.js
+++ app/js_src/basket.js
@@ -37,36 +37,14 @@ jQuery(document).ready(function () {
          return false;
       }
    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
    /*count minus*/
-   /*$(".count__minus").on("click", function (event) {
+   $(".count__minus").on("click", function (event) {
       event.preventDefault();
       var counterMinus = $(this).closest(".tovar__count").find("._tovarCount");
-
-
-      counterMinus.val(+counterMinus.val() - 1);
-
-
+      var countMinus = parseInt(counterMinus.val()) - 1;
+      countMinus = countMinus < 1 ? 1 : countMinus;
+      counterMinus.val(countMinus);
+      counterMinus.change();
       var tovarPriceM = $(this).closest(".tovar__count")
          .closest(".tovar__block--content").find("._tovarPrice").text();
       var tovarCountM = $(this).closest(".tovar__count").find("._tovarCount").val().trim();
@@ -74,18 +52,23 @@ jQuery(document).ready(function () {
       var tovarSummM = $(this).closest(".tovar__count")
          .closest(".tovar__block--content").find("._tovarSumm");
       tovarSummM.html(tovarSumm);
+      allSumm();
+      return false;
    });
    /*count plus*/
-   /*$(".count__plus").on("click", function (event) {
+   $(".count__plus").on("click", function (event) {
       event.preventDefault();
       var counertPlus = $(this).closest(".tovar__count").find("._tovarCount");
-      counertPlus.val(+counertPlus.val() + 1);
-      var tovarPriceM = $(this).closest(".tovar__count")
+      counertPlus.val(parseInt(counertPlus.val()) + 1);
+      counertPlus.change();
+      var tovarPriceP = $(this).closest(".tovar__count")
          .closest(".tovar__block--content").find("._tovarPrice").text();
-      var tovarCountM = $(this).closest(".tovar__count").find("._tovarCount").val().trim();
-      var tovarSumm = tovarPriceM * tovarCountM;
-      var tovarSummM = $(this).closest(".tovar__count")
+      var tovarCountP = $(this).closest(".tovar__count").find("._tovarCount").val().trim();
+      var tovarSummP = tovarPriceP * tovarCountP;
+      var tovarSummPblock = $(this).closest(".tovar__count")
          .closest(".tovar__block--content").find("._tovarSumm");
-      tovarSummM.html(tovarSumm);
-   });*/
+      tovarSummPblock.html(tovarSummP);
+      allSumm();
+      return false;
+   });
 });
